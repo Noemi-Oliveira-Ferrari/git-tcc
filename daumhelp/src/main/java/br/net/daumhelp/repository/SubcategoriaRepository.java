290db5bafda25a2417e4258b6a42ce3221fc93c1
Kelvin Ronaldo <40304152+KelvinRonaldo@users.noreply.github.com>
2019-08-29 20:45:28 -0300

    Revert "Card 05"

--- daumhelp/src/main/java/br/net/daumhelp/repository/SubcategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.net.daumhelp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.net.daumhelp.model.Subcategoria;
-
-public interface SubcategoriaRepository extends JpaRepository<Subcategoria, Long> {
-
-}
