eb69d994f80c32ab72264fc5a3bf916fe800bce2
Kelvin Ronaldo <kelvin.ronaldo@hotmail.com>
2019-09-03 23:43:02 -0300

    Telas de dados pessoais, confirm. email, endereco, serviço prontas; consumindo API

--- mobile/GIT-TCC/mobile/app/src/main/java/br/senai/sp/daumhelp/CadastroProfissionalActivity4.java
+++
@@ -1,8 +0,0 @@
-package br.senai.sp.daumhelp;
-
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
-
-public class CadastroProfissionalActivity4 extends AppCompatActivity {
-
-}
