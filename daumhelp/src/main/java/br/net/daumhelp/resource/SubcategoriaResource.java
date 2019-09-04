995a4b128f8249fb118a96dcda8e424608e21591
Kelvin Ronaldo <kelvin.ronaldo@hotmail.com>
2019-09-04 00:13:16 -0300

    API de cadastro e localidades finalizada

---
+++ daumhelp/src/main/java/br/net/daumhelp/resource/SubcategoriaResource.java
@@ -0,0 +1,42 @@
+package br.net.daumhelp.resource;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.net.daumhelp.model.Subcategoria;
+import br.net.daumhelp.repository.SubcategoriaRepository;
+
+@CrossOrigin(origins = "http://localhost")
+@RestController
+@RequestMapping("/subcategorias")
+public class SubcategoriaResource {
+	
+	@Autowired
+	private SubcategoriaRepository subcatRepository;
+
+	@GetMapping
+	public List<Subcategoria> getSubcategorias(){
+		return subcatRepository.findAll();
+	}
+
+	@GetMapping("/categoria/{idCategoria}")
+	public List<Subcategoria> getSubcatsByCat(@PathVariable Long idCategoria){
+		return subcatRepository.findByCategoria(idCategoria);
+	}
+	
+	@GetMapping("/id/{id}")
+	public Optional<Subcategoria> getSubcatsById(@PathVariable Long id){
+		System.out.println(subcatRepository.findById(id));
+		return subcatRepository.findById(id);
+	}
+		
+	
+	
+}
