3a153343f2b8ccf60880ff1392f16f45b340f62f
Kelvin Ronaldo <kelvin.ronaldo@hotmail.com>
2019-09-03 23:07:13 -0300

    Tela de escolha, faltam: trocar imgs e animações

--- daumhelp/src/main/java/br/net/daumhelp/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.net.daumhelp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.net.daumhelp.model.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-
-}
