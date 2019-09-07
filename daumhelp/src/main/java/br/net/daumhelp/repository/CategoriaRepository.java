763b49313093db6cb2cbe1465bc76a924d8b7116
Kelvin Ronaldo <kelvin.ronaldo@hotmail.com>
2019-09-07 20:48:04 -0300

    Categorias e Subcategorias estão ordenadas por nome na busca

--- daumhelp/src/main/java/br/net/daumhelp/repository/CategoriaRepository.java
+++ daumhelp/src/main/java/br/net/daumhelp/repository/CategoriaRepository.java
@@ -1,9 +1,13 @@
 package br.net.daumhelp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.net.daumhelp.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-
+	@Query("SELECT c FROM Categoria c WHERE c.idCategoria > 0 ORDER BY c.categoria")
+	public List<Categoria> findAllOrderByName();
 }
