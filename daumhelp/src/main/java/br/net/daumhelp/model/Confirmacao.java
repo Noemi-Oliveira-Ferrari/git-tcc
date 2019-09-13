b916e4cfe5c7ea30f9ba3fda1ac6b48c2a672938
Kelvin Ronaldo <kelvin.ronaldo@hotmail.com>
2019-09-13 01:00:25 -0300

    Implementando html em email com commons

--- daumhelp/src/main/java/br/net/daumhelp/model/Confirmacao.java
+++ daumhelp/src/main/java/br/net/daumhelp/model/Confirmacao.java
@@ -4,25 +4,37 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class Confirmacao {
-	
+
 	@NotNull
-	private String remetente;
+	private String nome;
+	@NotNull
+	private String destinatario;
 	@NotNull
 	@Size(min = 4, max = 4)
 	private String codigoConfirm;
-	
-	public String getRemetente() {
-		return remetente;
+
+	public String getNome() {
+		return nome;
 	}
-	public void setRemetente(String remetente) {
-		this.remetente = remetente;
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
+
+	public String getDestinatario() {
+		return destinatario;
+	}
+
+	public void setDestinatario(String destinatario) {
+		this.destinatario = destinatario;
+	}
+
 	public String getCodigoConfirm() {
 		return codigoConfirm;
 	}
+
 	public void setCodigoConfirm(String codigoConfirm) {
 		this.codigoConfirm = codigoConfirm;
 	}
-	
-	
+
 }
