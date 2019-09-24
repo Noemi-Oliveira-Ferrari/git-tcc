6905fd0421761bc695c1878785fecb12cca7f87a
Ester <estervitorr@gmail.com>
2019-09-24 14:03:37 -0300

    Componentizados: Confirmaçao com props do titulo e texto. Finalizada.

--- react/src/components/Login.js
+++ react/src/components/Login.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import DaUmHelp from './DaUmHelp';
+import InputLogin from './InputLogin';
+import LinkCadastro from './LinkCadastro';
+import Logo from './Logo';
+import Botao from './Botao';
 
 
 export class Login extends Component{
    render(){
     return(
-        
+         <div className="container-centro">
+            <div className="caixa-texto">
+               <div className="conteudo-caixa-texto">
+                  <h2>Comece a utilizar nossos serviços</h2>
+               </div>
+            </div>
+            <div className="caixa-conteudo">
+                  <div className="card-login">
+                     <div className="conteudo-card">
+
+                        <Logo
+                           classLogo="logotipo">
+                        </Logo>
+                              
+                        <DaUmHelp
+                           classNome="texto-centro titulo">
+                        </DaUmHelp>
+
+                           <form>
+                              <div className="texto-centro">
+
+                                 <InputLogin
+                                    label="E-mail"
+                                    type="email">
+                                 </InputLogin>
+
+                                 <InputLogin
+                                    label="Senha"
+                                    type="password">
+                                 </InputLogin>
+
+                                 <Botao
+                                    classBotao="btn-entrar"
+                                    valueBotao="Entrar">
+                                 </Botao> 
+                              </div>
+                           </form>
+                           <LinkCadastro>
+                           </LinkCadastro>
+                     </div>
+                  </div>
+               </div>
+         </div>
         
 
     );
