99ad1b476b1b54ecbe7deaa47f98cc346c7c871e
Kelvin Ronaldo <kelvin.ronaldo@hotmail.com>
2019-11-18 22:05:30 -0300

    Primeiro emit de teste

--- apiSocket/server.js
+++ apiSocket/server.js
@@ -1,18 +1,37 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 
 const server = require("http").Server(app);
 const io = require("socket.io").listen(server);
 
+const DOMINIO = "http://localhost:8080/";
+const porta = 8081;
+
 io.on("connection", function(socket) {
+
     console.log(`Socket ${socket.id} conectado!`);
 
-    socket.on("testando", data =>{
-        console.log(data);
+
+    socket.on("testando", dados =>{
+        console.log("____"+dados);
+    })
+
+    socket.on("solicitarPedido", pedido =>{
+        axios({
+            method: "POST",
+            url: `${DOMINIO}pedidos/solicitar`,
+            timeout: 30000,
+            data: pedido
+        })
+        .then(response =>{
+            socket.emit(`pedidoSolicitado-${pedido.profissional.idProfissional}`, pedido);
+        });            
     });
+
 });
 
 // var corsOptions = {
@@ -29,4 +48,6 @@ app.get("/teste", (res, req) => {
     console.log("teste");
 });
 
-server.listen(8081);
+server.listen(porta, ()=>{
+    console.log("Node Socket ouvindo a porta "+porta);
+});
